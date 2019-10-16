f805bb4b786fef588577672353b58818fe3f7b4c
NicolasAsis <nico.asis15@gmail.com>
2019-10-16 11:20:12 -0700

    Added Comps in Main

--- comps/Main.js
+++ comps/Main.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react';
 import {View,Text} from 'react-native';
 import Header from './Header';
+import Story from './Story';
+import Footer from './Footer';
 
 const Main = ()=>{
     return (
       <View> 
           <Header />
+          <Story />
+          <Footer />
       </View>
     )
   }
