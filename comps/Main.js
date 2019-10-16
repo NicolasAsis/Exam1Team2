a509cb98a522d1d70c029e59dd8a5a80a1672bfc
NicolasAsis <nico.asis15@gmail.com>
2019-10-15 18:55:33 -0700

    Added all React Files, made 'comps' and 'styles' folder

---
+++ comps/Main.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {View,Text} from 'react-native';
+
+const Main = ()=>{
+    return (
+      <View> 
+
+      </View>
+    )
+  }
+
+export default Main;
