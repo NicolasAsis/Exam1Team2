7ab49047d8cfb56dc1da24ec9f50a00d55c499ba
Dora Tao <tsy6808@gmail.com>
2019-10-16 11:20:05 -0700

    Styles for Story

--- styles/StoryStyles.js
+++ styles/StoryStyles.js
@@ -1,18 +1,28 @@
 import {StyleSheet} from 'react-native';
 
 var styles ={
+  scrollView: {
+  },
+
 bg:{
   alignItems:"center",
   justifyContents:"center",
   padding:10,
   flexDirection:"row",
-
 },
+
 switch:{
   size:20,
   top:80,
-  color:"green"
+  color:"green",
+  alignContent: 'center',
+  justifyItems: 'center'
+},
+
+text: {
+  fontSize: 35
 }
+
 };
 
 export default styles;
